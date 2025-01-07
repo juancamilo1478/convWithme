e8608d17de22652ed43222acacf5579a4440eee2
juanc <juancamilo1478@gmail.com>
2025-01-07 16:18:21 -0500

    save

--- src/views/CardItem.vue
+++ src/views/CardItem.vue
@@ -1,12 +1,12 @@
 <template>
-    <div class="max-w-xs sm:max-w-sm md:max-w-md lg:max-w-lg xl:max-w-xl  bg-brand-colorOne py-10 px-10 rounded-lg shadow-md flex flex-col items-center ">
+    <div class="max-w-xs sm:max-w-sm md:max-w-md lg:max-w-lg xl:max-w-xl  bg-brand-colorOne py-10 px-2 md:px-10 lg:px-10 rounded-lg shadow-md flex flex-col items-center ">
       <!-- Icono renderizado desde el slot -->
       <div class="icon mb-2">
         <slot name="icon"></slot>
       </div>
       <!-- Texto renderizado desde la prop -->
       <div class="text text-center text-white">
-        <h1 class="text-2xl py-10"> {{ text }}</h1>
+        <h1 class="text-2xl sm:text-xl py-10"> {{ text }}</h1>
       </div>
     </div>
   </template>
