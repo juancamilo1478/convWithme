4d745f31f01f9810a9a443cb0d5060e115ff3372
juanc <juancamilo1478@gmail.com>
2025-01-17 22:01:37 -0500

    save

--- src/views/video.vue
+++ src/views/video.vue
@@ -7,7 +7,7 @@
             <div class="relative w-full overflow-hidden rounded-lg shadow-lg" style="padding-top: 56.25%;">
                     <iframe
                       class="absolute top-0 left-0 w-full h-full"
-                      src="https://www.youtube.com/embed/Qs2-klYtq5Y"
+                      src="https://www.youtube.com/embed/-49XOQvoX9I"
                       title="YouTube video"
                       frameborder="0"
                       allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
